8a797b39cfaed39f72878ce58ca0d32993e35b37
Aniket <moteaniket952@gmail.com>
2025-09-14 09:02:07 +0530

    key hide

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6KpjURgs_mSupculV_fuXdvDiYQY0k6s",
-  authDomain: "netflixgpt-bf19c.firebaseapp.com",
-  projectId: "netflixgpt-bf19c",
-  storageBucket: "netflixgpt-bf19c.firebasestorage.app",
-  messagingSenderId: "285962700128",
-  appId: "1:285962700128:web:c684699d8af32f64af0a05",
-  measurementId: "G-SCYH8EH40H"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth(app)
+export const auth = getAuth(app);
